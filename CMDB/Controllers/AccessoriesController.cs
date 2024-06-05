acfe92d63c37e167972c51293c86490aa7bb4d1e
witekmikolajczak <witekmikolajczak@gmail.com>
2024-06-05 11:49:59 +0200

    feat: add pagination

--- CMDB/Controllers/AccessoriesController.cs
+++ CMDB/Controllers/AccessoriesController.cs
@@ -1,6 +1,6 @@
 ﻿using CMDB.Data;
 using CMDB.Models.DBEntities;
-using CMDB.Models;
+using CMDB.ViewModels; // PAGINATION
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,85 +11,130 @@ namespace CMDB.Controllers
 	public class AccessoriesController : Controller
 	{
 		private readonly AccesoriesDbContext _context;
+		private readonly int[] pageSizeOptions = { 5, 10, 20 }; // PAGINATION
 
 		public AccessoriesController(CMDB.Data.AccesoriesDbContext context)
 		{
 			_context = context;
 		}
 
-		[HttpGet]
-		public IActionResult Index()
-		{
-			var accessories = _context.Accessories.ToList();
-			var accessoryList = new List<AccessoriesViewModel>();
+      [HttpGet]
+		 public IActionResult Index(int page = 1, int pageSize = 5)
+        {
+            var accessories = _context.Accessories
+                                    .Skip((page - 1) * pageSize)
+                                    .Take(pageSize)
+                                    .ToList();
 
-			if (accessories != null)
-			{
-				foreach (var accessory in accessories)
-				{
-					var accessoryViewModel = new AccessoriesViewModel()
-					{
-						AccessoryID = accessory.AccessoryID,
-						Name = accessory.Name,
-						Type = accessory.Type,
-						Manufacturer = accessory.Manufacturer,
-						SerialNumber = accessory.SerialNumber,
-						PurchaseDate = accessory.PurchaseDate,
-						WarrantyExpiration = accessory.WarrantyExpiration,
-						Employees = accessory.Employees
-					};
+            var totalCount = _context.Accessories.Count();
+            ViewData["Pager"] = new PagerViewModel(page, pageSize, totalCount, pageSizeOptions);
 
-					accessoryList.Add(accessoryViewModel);
-				}
+            return View(accessories);
+        }
 
-				return View(accessoryList);
-			}
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var accessory = _context.Accessories.Find(id);
+            if (accessory == null)
+            {
+                TempData["errorMessage"] = "accessory not found.";
+                return RedirectToAction("Index");
+            }
+            return View(accessory);
+        }
 
-			return View(accessoryList);
-		}
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
 
-		[HttpGet]
-		public IActionResult Create()
-		{
-			return View();
-		}
+        [HttpPost]
+        public IActionResult Create(Accessories accessory)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["errorMessage"] = "Model data is not valid.";
+                return View(accessory);
+            }
 
-		[HttpPost]
-		public IActionResult Create(AccessoriesViewModel accessoryData)
-		{
-			try
-			{
-				if (ModelState.IsValid)
-				{
-					var accessory = new Accessories()
-					{
-						AccessoryID = accessoryData.AccessoryID,
-						Name = accessoryData.Name,
-						Type = accessoryData.Type,
-						Manufacturer = accessoryData.Manufacturer,
-						SerialNumber = accessoryData.SerialNumber,
-						PurchaseDate = accessoryData.PurchaseDate,
-						WarrantyExpiration = accessoryData.WarrantyExpiration,
-						Employees = accessoryData.Employees
-					};
+            try
+            {
+                _context.Accessories.Add(accessory);
+                _context.SaveChanges();
 
-					_context.Accessories.Add(accessory);
-					_context.SaveChanges();
+                TempData["successMessage"] = "accessory created successfully.";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return View(accessory);
+            }
+        }
 
-					TempData["successMessage"] = "Accessory created successfully.";
-					return RedirectToAction("Index");
-				}
-				else
-				{
-					TempData["errorMessage"] = "Model data is not valid.";
-					return View();
-				}
-			}
-			catch (Exception ex)
-			{
-				TempData["errorMessage"] = ex.Message;
-				return View();
-			}
-		}
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var accessory = _context.Accessories.Find(id);
+            if (accessory == null)
+            {
+                TempData["errorMessage"] = "accessory not found.";
+                return RedirectToAction("Index");
+            }
+            return View(accessory);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Accessories accessory)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Accessories.Update(accessory);
+                    _context.SaveChanges();
+
+                    TempData["successMessage"] = "accessory updated successfully.";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    TempData["errorMessage"] = "Model data is not valid.";
+                    return View(accessory);
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return View(accessory);
+            }
+        }
+        
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                var accessory = _context.Accessories.Find(id);
+                if (accessory == null)
+                {
+                    TempData["errorMessage"] = "accessory not found.";
+                    return RedirectToAction("Index");
+                }
+
+                _context.Accessories.Remove(accessory);
+                _context.SaveChanges();
+
+                TempData["successMessage"] = "accessory deleted successfully.";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["errorMessage"] = ex.Message;
+                return RedirectToAction("Index");
+            }
+        }
 	}
 }
